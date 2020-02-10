d7c39659c1e1d962b16ed64713d596961a97435a
Sushma Yadupathi <syadupathi@salesforce.com>
2020-02-10 15:06:54 -0500

    moved product avatar to header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ProductAvatar from "./components/ProductAvatar/ProductAvatar";
 function App() {
   return (
     <div className="App">
-        <ProductAvatar source="https://i.pinimg.com/originals/fd/45/08/fd45084c3a469854444bb5ff619f2d8d.png"/>
       <ProductDetailHeader />
     </div>
   );
--- src/components/ProductDetailHeader/ProductDetailHeader.js
+++ src/components/ProductDetailHeader/ProductDetailHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import ProductAvatar from "../ProductAvatar/ProductAvatar";
 
 const ProductDetailHeader = props => {
     return (
-        <div> 
+        <div>
+            <ProductAvatar source="https://i.pinimg.com/originals/fd/45/08/fd45084c3a469854444bb5ff619f2d8d.png"/>
             <div>
             <Button variant="contained">Default</Button>
             <Button variant="contained">Task</Button>

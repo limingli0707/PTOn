8a90609fc56656bd68160ed4941a4109768872ae
Mingli Li <mingli.li@salesforce.com>
2020-02-10 23:19:28 -0500

    app exchange

---
+++ src/components/AppList/data.js
@@ -0,0 +1,58 @@
+import uuid from 'uuid/v1';
+
+export default [
+  {
+    id: uuid(),
+    title: 'Promotion Expert',
+    description:
+      'Promotion Expert is an easy-to-use tool to manage promotions',
+    imageUrl: '/images/apps/shopping-cart-promotion.png',
+    totalDownloads: '594',
+    updatedAt: '27/03/2019'
+  },
+  {
+    id: uuid(),
+    title: 'Tivo Campaigns App',
+    description:
+      'Tivo campaigns helps in automating your email marketing, where every email can be measured & tracked effectively.',
+    imageUrl: '/images/apps/Campaigns-icon.png',
+    totalDownloads: '625',
+    createdAt: '31/03/2019'
+  },
+  {
+    id: uuid(),
+    title: 'Catalog Machine',
+    description:
+      'Fast market response - prepare catalogs in minutes.One-button click to import and sync products',
+    imageUrl: '/images/apps/Catalog_Machine.png',
+    totalDownloads: '857',
+    createdAt: '03/04/2019'
+  },
+  {
+    id: uuid(),
+    title: 'Wish Marketplace',
+    description:
+      'List, Sync, Manage, & Automate to boost sales on Wish. Import, export or sync your B2C Commerce products with Wish.',
+    imageUrl: '/images/apps/Wish_Marketplace.png',
+    totalDownloads: '406',
+    createdAt: '04/04/2019'
+  },
+  {
+    id: uuid(),
+    title: 'Bulk edit products',
+    description:
+      'Bulk edit thousands of products, CSV & Excel import bulk edit. Filter products/variants to edit by collection, product type, vendor, tags or any combination of these.',
+    imageUrl: '/images/apps/BPE.png',
+    totalDownloads: '835',
+    createdAt: '04/04/2019'
+  },
+  {
+    id: uuid(),
+    title: 'Page Speed Optimizer Pro',
+    description:
+      'Speed Up your B2C Commerce store in 60 Seconds by installing our app and turning it on! One click installation. No coding needed!',
+    imageUrl: '/images/apps/Page_Speed_Optimizer_Pro.png',
+    totalDownloads: '835',
+    createdAt: '04/04/2019'
+  }
+];

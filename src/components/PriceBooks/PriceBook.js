2aaf719ace3de2fd5a1a8c839427d635700ac210
Kushang Gajjar <kgajjar@salesforce.com>
2020-02-13 18:08:35 -0500

    PDP

--- src/components/PriceBooks/PriceBook.js
+++ src/components/PriceBooks/PriceBook.js
@@ -2,8 +2,30 @@ import React from "react";
 import { Card, CardHeader, CardContent } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import { blue } from "@material-ui/core/colors";
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
-const PriceBook = () => {
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  })
+);
+
+const PriceBook = ({ products }) => {
+  const classes = useStyles();
   return (
     <Card>
       <CardHeader
@@ -15,9 +37,9 @@ const PriceBook = () => {
           </Avatar>
         }
       ></CardHeader>
-       <CardContent>
-          
-        </CardContent>
+      <CardContent>
+
+      </CardContent>
     </Card>
   );
 };
